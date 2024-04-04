7c38bd850914f60343c1e7d2b6b7b9bd0961cd46
letieu <letieu8@gmail.com>
2024-04-04 10:43:48 +0700

    feat: add comment to insertion sort

--- insertion/insertion.go
+++ insertion/insertion.go
@@ -5,11 +5,14 @@ func Sort(arr []int) []int {
 		key := arr[i]
 		j := i - 1
 
+		// Move elements of arr[0...i-1], that are greater than key, to one position ahead of their current position.
+		// This loop will keep shifting elements to the right until it finds the correct position for the key.
 		for j >= 0 && arr[j] > key {
 			arr[j+1] = arr[j]
 			j--
 		}
 
+		// Place the key in its correct location in the sorted sequence.
 		arr[j+1] = key
 	}
 
