966099de63bdc77dda48ceb358d6d75df98d5c87
letieu <letieu8@gmail.com>
2024-04-04 11:03:26 +0700

    feat: selection sort

--- selection/selection.go
+++ selection/selection.go
@@ -1,5 +1,17 @@
 package selection
 
 func Sort(arr []int) []int {
+	for i := 0; i < len(arr)-1; i++ {
+		minIndex := i
+
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+
 	return arr
 }
